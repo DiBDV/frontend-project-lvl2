af350f5bd7ec7dee3dd90ed6815e3cc57b88c705
DiBDV <bdv.dmitry@gmail.com>
2022-01-13 21:52:39 +0100

    Correct commander functionality
    Corected commander.js related code to properly show the help output

--- gendiff.js
+++ gendiff.js
@@ -5,12 +5,10 @@
 const { Command } = require ('commander');
 const program = new Command();
 
+
 program
   .version('0.0.1')
-  .name("gendiff")
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number')
-  .option('-h, --help', 'output usage information');
-
+  .parse();
 
 
