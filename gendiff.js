22a54945d13033a8281c776724ff70518540f000
DiBDV <bdv.dmitry@gmail.com>
2022-01-13 15:09:25 +0100

    Implement commander cli functionality
    Implemented commander functionality. Created initial config

--- gendiff.js
+++ gendiff.js
@@ -1,1 +1,17 @@
 #!/usr/bin/env node
+
+//commander.js to generate helper functionality
+
+const commander = require('../'); // to include commander in git clone
+const program = new commnader.Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .name("gendiff")
+  .usage("[options]")
+  .option('-V, --version', 'output the version number')
+  .option('-h, --help', 'output usage information');
+
+program.parse(process.argv);
+
+
