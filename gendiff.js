84cd493bf7dc4af4b8af7f07021936e4ffc80a7c
DiBDV <bdv.dmitry@gmail.com>
2022-01-14 15:10:23 +0100

    Add filepath arguments
    Added filepath arguments to the gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -9,6 +9,12 @@ const program = new Command();
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>', 'the path to the first file')
+  .argument('<filepath2>', 'the path to the second file')
+  //.command('<filepath1>', '<filepath2>')
+  .option('-f, --format [type]', 'output format')
+  //.action((filepath1, filepath2) => {   
+   // })
   .parse();
 
 
