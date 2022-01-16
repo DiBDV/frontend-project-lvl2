280330a0fa4365ef276804843412593c504c3cca
DiBDV <bdv.dmitry@gmail.com>
2022-01-16 13:07:46 +0100

    Import fs module from Node.js

--- gendiff.js
+++ gendiff.js
@@ -1,19 +1,28 @@
 #!/usr/bin/env node
 
+import { readFileSync } from 'fs';
+
 //commander.js integration to generate helper functionality
 
 const { Command } = require ('commander');
 const program = new Command();
 
 
+program.version('0.0.1');
+
 program
-  .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .option('-f, --format [type]', 'output format')
-  //.action((filepath1, filepath2) => {   
-   // })
-  .parse();
+  .action( (filepath1, filepath2) => {   
+    // const filepath1 = JSON.parse(file1)
+    const file1 = fs.readFileSync(filepath1)
+    console.log(file1)
+    const file2 = fs.readFileSync(filepath2)
+    console.log(file2);
+  });
+
+program.parse(process.argv);
 
 
