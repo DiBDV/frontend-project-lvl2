4ac749c2f0deafb75d5c09e5e081ea71e47618ac
DiBDV <bdv.dmitry@gmail.com>
2022-04-04 20:12:16 +0200

    Adding latest changes

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,30 @@
 #!/usr/bin/env node
 
-import { program } from '../src/index.js';
+import { Command } from 'commander';
+import { getFileContent } from '../src/get-file-content.js';
+import { parse, buildDiff, renderDiff } from '../src/index.js';
 
-program();
+
+//commander.js integration to generate helper functionality
+const program = new Command();
+
+
+program.version('0.1.0');
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action( (filepath1, filepath2) => {
+    const file1Content = getFileContent(filepath1);
+    const file2Content = getFileContent(filepath2);
+
+    const obj1 = parse(file1Content);
+    const obj2 = parse(file2Content);
+    const diff = buildDiff(obj1, obj2);
+    const result = renderDiff(diff, program.format);
+    console.log(result);
+  });
+
+program.parse(process.argv);
