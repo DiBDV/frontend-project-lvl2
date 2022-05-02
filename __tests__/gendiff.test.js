944e87655cc23cb059a01f06a71bc258397301ed
DiBDV <bdv.dmitry@gmail.com>
2022-05-02 17:52:36 +0200

    Add fixtures for recursion files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -14,8 +14,8 @@ test("gendiff flat file", () => {
     });
 
 test("gendiff recursion", () => {
-    expect(gendiff("__tests__/__fixtures__/file1.json", "__tests__/__fixtures__/file2.json", "json")).toBe(
+    expect(gendiff("__tests__/__fixtures__/file1_recursion.json", "__tests__/__fixtures__/file2_recursion.json", "json")).toBe(
       parse("__tests__/__fixtures__/expected_02.txt", "json"));
-    expect(gendiff("__tests__/__fixtures__/file1.yaml", "__tests__/__fixtures__/file2.yaml", "yaml")).toBe(
+    expect(gendiff("__tests__/__fixtures__/file1_recursion.yaml", "__tests__/__fixtures__/file2_recursion.yaml", "yaml")).toBe(
       parse("__tests__/__fixtures__/expected_02.txt", "yaml"));
     });
