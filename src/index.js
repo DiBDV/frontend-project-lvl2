aaec39b9393605ba7fc0f690aa910b477eb5564d
DiBDV <bdv.dmitry@gmail.com>
2022-05-17 21:11:12 +0200

    Replace hasOwn with Lodash equivalent
    Replaced hasOwn() to _.has in src/index.js

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ export const buildDiff = (data1, data2) => {
   for (const key of keys) {
     if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
        result[key] = {type: "nested", value: buildDiff(data1[key], data2[key])};
-    } else if (!Object.hasOwn(data1, key)) {
+    } else if (!_.has(data1, key)) {
       result[key] = {type: "added", value: data2[key]};
-    } else if (!Object.hasOwn(data2, key)) {
+    } else if (!_.has(data2, key)) {
       result[key] = {type: "deleted", value: data1[key]} ;
     } else if (!_.isEqual(data1[key], data2[key])) {
       result[key] = {type: "changed", value: [data1[key], data2[key]]};
