88c0cac9c67655677cc922fe810e3e0983c60a8a
DiBDV <bdv.dmitry@gmail.com>
2022-05-26 20:50:42 +0200

    Remove flat file tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,11 +8,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.resolve(__dirname, '..', '__tests__/__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('gendiff flat file', () => {
-  expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish')).toBe(readFile('expected_01.txt'));
-  expect(gendiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'stylish')).toBe(readFile('expected_01.txt'));
-});
-
 test('gendiff recursion stylish', () => {
   expect(gendiff(getFixturePath('file1_recursion.json'), getFixturePath('file2_recursion.json'), 'stylish')).toBe(readFile('expected_02_stylish.txt'));
   expect(gendiff(getFixturePath('file1_recursion.yml'), getFixturePath('file2_recursion.yml'), 'stylish')).toBe(readFile('expected_02_stylish.txt'));
