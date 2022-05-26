39b00824138d2a90ed2e31d35a915dd0e03a3d10
DiBDV <bdv.dmitry@gmail.com>
2022-05-26 21:52:00 +0200

    Use test.each() and reduce code length

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,17 +8,11 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.resolve(__dirname, '..', '__tests__/__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('gendiff recursion stylish', () => {
-  expect(gendiff(getFixturePath('file1_recursion.json'), getFixturePath('file2_recursion.json'), 'stylish')).toBe(readFile('expected_02_stylish.txt'));
-  expect(gendiff(getFixturePath('file1_recursion.yml'), getFixturePath('file2_recursion.yml'), 'stylish')).toBe(readFile('expected_02_stylish.txt'));
-});
-
-test('gendiff recursion plain', () => {
-  expect(gendiff(getFixturePath('file1_recursion.json'), getFixturePath('file2_recursion.json'), 'plain')).toBe(readFile('expected_03_plain.txt'));
-  expect(gendiff(getFixturePath('file1_recursion.yml'), getFixturePath('file2_recursion.yml'), 'plain')).toBe(readFile('expected_03_plain.txt'));
-});
-
-test('gendiff recursion json', () => {
-  expect(gendiff(getFixturePath('file1_recursion.json'), getFixturePath('file2_recursion.json'), 'json')).toBe(readFile('expected_04_json.txt'));
-  expect(gendiff(getFixturePath('file1_recursion.yml'), getFixturePath('file2_recursion.yml'), 'json')).toBe(readFile('expected_04_json.txt'));
+test.each([
+  ['file1_recursion.json', 'file2_recursion.json', 'stylish', 'expected_02_stylish'],
+  ['file1_recursion.yml', 'file2_recursion.yml', 'plain', 'expected_03_plain'],
+  ['file1_recursion.yml', 'file2_recursion.json', 'json', 'expected_04_json'],
+])('Show diff %s vs %s with %s formatting', (file1, file2, format, expectedResult) => {
+  const result = gendiff(getFixturePath(file1), getFixturePath(file2), format);
+  expect(result).toBe(readFile(`${expectedResult}.txt`));
 });
